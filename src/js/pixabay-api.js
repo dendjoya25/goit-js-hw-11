5e49ec1458dd307e81e883ce5e8c6373643e590c
Den_Djoya <djoya.den@gmail.com>
2024-08-19 22:43:11 +0200

    change code, and add new styles

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,15 +1,16 @@
 const BASE_URL = 'https://pixabay.com/api/';
 
 export const fetchPhotos = searchedQuery => {
-  const ulrParams = new URLSearchParams({
+  const urlParams = new URLSearchParams({
     key: '45497823-3ee11a5b90ff874601dfe7178',
     q: searchedQuery,
     image_type: 'photo',
     orientation: 'horizontal',
     safesearch: 'true',
+    per_page: 18,
   });
 
-  return fetch(`${BASE_URL}?${ulrParams}`).then(response => {
+  return fetch(`${BASE_URL}?${urlParams}`).then(response => {
     if (!response.ok) {
       throw new Error(response.status);
     }
--- src/main.js
+++ src/main.js
@@ -1,19 +1,33 @@
-import { createGalleryCardTamplate } from './js/render-functions';
-import { fetchPhotos } from './js/pixabay-api';
+import {
+  createGalleryCardTemplate,
+  initializeLightbox,
+} from './js/render-functions';
 
+import { fetchPhotos } from './js/pixabay-api';
 import iziToast from 'izitoast';
 
 const formEl = document.querySelector('.js-page-form');
 const galleryEl = document.querySelector('.js-gallery');
+const loaderEl = document.querySelector('.js-loader');
+
+const showLoader = () => {
+  loaderEl.classList.remove('is-hidden');
+};
+
+const hideLoader = () => {
+  loaderEl.classList.add('is-hidden');
+};
 
 const onFormElSubmit = event => {
   event.preventDefault();
 
   const searchedValue = formEl.elements.user_query.value;
 
+  showLoader();
+
   fetchPhotos(searchedValue)
     .then(data => {
-      if (data.hits.length === 0) {
+      if (data.hits && data.hits.length === 0) {
         iziToast.error({
           message:
             'Sorry, there are no images matching your search query. Please try again!',
@@ -24,14 +38,23 @@ const onFormElSubmit = event => {
         return;
       }
 
-      const galleryCardsTamplate = data.hits
-        .map(imgDetails => createGalleryCardTamplate(imgDetails))
+      const galleryCardsTemplate = data.hits
+        .map(imgDetails => createGalleryCardTemplate(imgDetails))
         .join('');
 
-      galleryEl.innerHTML = galleryCardsTamplate;
+      galleryEl.innerHTML = galleryCardsTemplate;
+
+      initializeLightbox();
     })
     .catch(err => {
       console.log(err);
+      iziToast.error({
+        message: 'Something went wrong. Please try again!',
+        position: 'topRight',
+      });
+    })
+    .finally(() => {
+      hideLoader();
     });
 };
 
